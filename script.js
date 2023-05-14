c415cbace8be70cfaf19eac4156bd51fb901361b
shrutijain2296 <128174123+shrutijain2296@users.noreply.github.com>
2023-05-14 18:56:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ function threeSum(arr, target) {
 		for(let j = i+1; j < arr.length-1; j++){
 			for(let k = j+1; k < arr.length; k++){
 				let sum = arr[i] + arr[j] + arr[k];
-				if(sum >= target){
-					ans = Math.min(sum, ans);
+				if(Math.abs(sum - target) < Math.abs(ans-target){
+					ans = sum
 				}
 			}
 		}
