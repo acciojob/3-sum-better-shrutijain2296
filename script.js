b9d8374a331ed006ded7ee5f3e748e7a02208574
shrutijain2296 <128174123+shrutijain2296@users.noreply.github.com>
2023-05-14 18:53:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 function threeSum(arr, target) {
 // write your code here
-	let sum = 0;
 	let ans = 0;
 	for(let i = 0; i < arr.length-2; i++){
 		for(let j = i+1; j < arr.length-1; j++){
 			for(let k = j+1; k < arr.length; k++){
-				sum = arr[i] + arr[j] + arr[k];
+				let sum = arr[i] + arr[j] + arr[k];
 				if(sum >= target){
 					ans = Math.min(sum, ans);
 				}
