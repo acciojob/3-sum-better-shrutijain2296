495781eff14caf999f64fa9d7b0c50e2d37c6d2f
shrutijain2296 <128174123+shrutijain2296@users.noreply.github.com>
2023-05-14 18:34:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 function threeSum(arr, target) {
 // write your code here
-  
+	let sum = 0;
+	for(let i = 0; i < arr.length; i++){
+		sum = sum + arr[i];
+		if(sum > target){
+			return;
+		}
+	}
+	return sum;
 }
+
 
 module.exports = threeSum;
